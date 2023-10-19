b7c1f8fe79b04ef679e6ba290e3401b3181a4d7a
Muhammad Maaz <75994722+maazmlr@users.noreply.github.com>
2023-10-19 14:27:48 +0500

    commited

--- src/components/Takaful_&_Insurence/Car Takaful/bike/AutoComp.jsx
+++ src/components/Takaful_&_Insurence/Car Takaful/bike/AutoComp.jsx
@@ -6,14 +6,14 @@ import { useState } from 'react';
 
 
 export default function ComboBox({label,data, handleChange}) {
-  const [carType,setCarType]=useState('');
+  const [BikeType,setbikeType]=useState('');
 
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
       options={data}
-      style={{width:"20rem",height:"3rem",marginBottom:'2rem'}}
+      style={{width:"21rem",height:"3rem",marginBottom:'2rem'}}
       renderInput={(params) => <TextField {...params} label={label} onBlur={()=>handleChange? handleChange(params.inputProps.value): ''} required/>}
     />
   );
--- src/components/Takaful_&_Insurence/Car Takaful/form.jsx
+++ src/components/Takaful_&_Insurence/Car Takaful/form.jsx
@@ -22,38 +22,41 @@ const tailLayout = {
   },
 };
 const App = () => {
- 
-
-
-
   const [form] = Form.useForm();
   const [curr, setCurr] = useState(0);
-  const [details, setDetails] = useState(null);
+  const [details, setdetails] = useState();
+  const [loginDetails, setLoginDetails] = useState();
+  const [profileDetails, setProfileDetails] = useState();
   const [Profile, setProfile] = useState(null)
-  const onGenderChange = (value) => {
-  };
+  console.log("LoginDetails",loginDetails)
+  console.log("profileDetails",profileDetails)
   const onFinishLoginForm = (values) => {
-    setDetails(values);
     setCurr(1);
   };
   const onFinishProfileForm = (values) => {
-    setDetails(values);
     setCurr(2);
   };
+  const updateValueLogin=(values)=>{
+    setLoginDetails(values)
+  }
+  const updateValueProfile=(values)=>{
+    setProfileDetails(values)
+  }
+  const finishBtn=()=>{
+   const data = {...loginDetails,...profileDetails}
+   console.log(data)
+  }
   const forms = [
-    <LoginForm onFinish={onFinishLoginForm} initialValues={details}/>,
-    <ProfileForm onFinish={onFinishProfileForm} initialValues={Profile} />,
-    <Finish/>
+    <LoginForm onFinish={onFinishLoginForm} initialValues={updateValueLogin}/>,
+    <ProfileForm onFinish={onFinishProfileForm} initialValues={updateValueProfile}/>,
+    <Finish onFinish={finishBtn}/>
 ]
-  const onReset = () => {
-    form.resetFields();
-  };
   const isStepDisabled=(stepNumber)=>{
     if(stepNumber === 0){
       return false;
     }
     if(stepNumber === 1){
-      return details === null
+      return details === null 
     }
     if(stepNumber === 2){
       return details === null || Profile === null
@@ -69,7 +72,7 @@ const App = () => {
         <Steps style={{padding: '32px 16px'}}
         onChange= {setCurr}
         current={curr}>
-          <Steps.Step title="login" disabled={isStepDisabled(0)} icon={<LoginOutlined/>}/>
+          <Steps.Step title="Login" disabled={isStepDisabled(0)} icon={<LoginOutlined/>}/>
           <Steps.Step title="Profile" disabled={isStepDisabled(1)} icon={<LoginOutlined/>}/>
           <Steps.Step title="Finsih" disabled={isStepDisabled(2)} icon={<LoginOutlined/>}/>
         </Steps>
@@ -79,28 +82,50 @@ const App = () => {
   )
 };
 function LoginForm({onFinish, initialValues}){
+  
   const [carData,setCarData]=useState([])
-
+  const [carName, setCarName] = useState()
+  const [carYear, setCarYear]= useState()
+  const [carModel, setCarModel]= useState()
+  const [carPrize, setCarPrize] = useState()
+  const [carType, setCarType]= useState([])
   useEffect(()=>{
     const val = axios.get("http://localhost:3000/car")
     val.then(res=>setCarData(res.data.message[0].carData))
   },[])
+  let updateValue = {
+    carModel,
+    carName,
+    carYear,
+    carPrize
+  }
 
-
+  console.log(carPrize)
 
   const nameCar=carData.map((v,i)=>(v.carName))
-    
-  const [carMan,setCarMan]=useState("")
 
-
-  function handleChange(e){
-    setCarMan(e)
-    console.log("car-->",carMan)
+  function handleChangeCarName(e){
+    setCarName(e)
   }
+  function handleChangeYear(e){
+    setCarYear(e)
+  } 
+  function handleChangeModel(e){
+    setCarModel(e)
+  }
+  let carTypeI=-1
+  if(carName){
+    carTypeI=carData.findIndex(v=>carName == v.carName)
+  }
+
+  useEffect(()=>{
+      setCarType(carData[carTypeI]?.details.map(v=>((v.modelName))))
+      console.log('basit')
+  },[carName])
+  const year =(carData[carTypeI]?.details[0].years)
+
   
   return(
-
-
     <Form
         {...layout}
         name="control-hooks"
@@ -113,72 +138,49 @@ function LoginForm({onFinish, initialValues}){
           marginRight: 10
         }}
       >
-        <Form.Item
-          name="manufacturer"
-
-          rules={[
-            {
-              required: false,
-            },
-          ]}
-        >
-        
-          <ComboBox  label={"select manufacturer"} data={nameCar}  handleChange={handleChange}/>
+        <Form.Item>
+          <ComboBox  label={"select manufacturer"} data={nameCar}  handleChange={handleChangeCarName}/>
         </Form.Item>
-        <Form.Item
-          name="model"
-          rules={[
-            {
-              required: false,
-            },
-          ]}
-        >
-           <ComboBox label={'select model'} data={nameCar}/>
+        <Form.Item>
+           <ComboBox label={'select model'} data={carType ? carType : []} handleChange={handleChangeModel}/>
         </Form.Item>
-        <Form.Item
-          name="manufacturing year"
-          rules={[
-            {
-              required: false,
-            },
-          ]}
-        >
-             <ComboBox label={'select manufacturing year'} data={nameCar} />
+        <Form.Item>
+             <ComboBox label={'select manufacturing year'} data={year ? year : []} handleChange={handleChangeYear}/>
         </Form.Item>
-        <Form.Item 
-        name="value of car"
-        rules={[
-          {
-            required: false,
-            
-          },
-        ]}
-        > <TextField
+        <Form.Item> 
+        <TextField
         id="outlined-number"
         label="Value of car"
         type="number"
         style={{width:'20rem'}}
-        InputLabelProps={{
-          shrink: true,
-        }}
+        InputProps={{ inputProps: { min: 400000, max: 40000000 } }}
+        onBlur={(e)=> setCarPrize(e.target.value)}
+        required
       />
         </Form.Item>
-        
         <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit"
+        <Button type="primary" htmlType='submit'
           style={{padding: '0rem 2rem'}}
-          className='form-btn'>
-            Next
+          className='form-btn'
+          onClick={()=> initialValues({...updateValue})}>
+            Continue
           </Button>
         </Form.Item>
       </Form>
   )
 }
 function ProfileForm({onFinish,  initialValues}){
+  const [fullName, setFullName] = useState()
+  const [email, setEmail] = useState()
+  const [phone, setPhone] = useState()
+  const updateValue = {
+    fullName,
+    email,
+    phone
+  }
   return(
     <Form 
     onFinish={onFinish}
-    initialValues={initialValues}
     style={{
       marginTop: 20,
       marginBottom: 20,
@@ -186,57 +188,50 @@ function ProfileForm({onFinish,  initialValues}){
       marginRight: 10
     }}
   >
-   <Form.Item 
-    name="Name"
-    rules={[
-      {
-        required: true,
-        
-      },
-    ]}
-    >
-      <Input placeholder='Full name' style={{width: '20rem', height: '3rem'}}/>
+   <Form.Item>
+      <TextField
+        label="Full name"
+        type={"text"}
+        style={{width: '20rem'}}
+        onChange={(e)=>setFullName(e.target.value)}
+      required/>
     </Form.Item>
-    <Form.Item 
-    name="Email address"
-    rules={[
-      {
-        required: true,
-        
-      },
-    ]}
-    >
-      <Input placeholder='Email address' style={{width: '20rem', height: '3rem'}}/>
+    <Form.Item>
+      <TextField
+        label="Email"
+        type={"email"}
+        style={{width: '20rem'}}
+        onChange={(e)=>setEmail(e.target.value)}
+      required/>
     </Form.Item>
-    <Form.Item 
-    name="Phone"
-    rules={[
-      {
-        required: true,
-        
-      },
-    ]}
-    >
-      <InputNumber placeholder='Phone' style={{width: '20rem', height: '3rem'}}/>
+    <Form.Item>
+      <TextField
+        id="outlined-number"
+        label="Value of car"
+        type="number"
+        style={{width:'20rem'}}
+        onChange={(e)=>setPhone(e.target.value)}
+        required
+      />
     </Form.Item>
     <Form.Item {...tailLayout}>
         <Button type="primary" htmlType="submit"
           style={{padding: '0rem 2rem'}}
-          onClick={()=> setValue(false)}
-          className='form-btn'>
+          className='form-btn'
+          onClick={()=>initialValues({...updateValue})}>
             See Plans
           </Button>
         </Form.Item>
     </Form>
   )
 }
-function Finish(){
+function Finish({onFinish}){
   return(
     <Button type="primary" htmlType="submit"
           style={{padding: '0rem 2rem'}}
-          onClick={()=> setValue(false)}
-          className='form-btn'>
-            Finish
+          className='form-btn'
+          onClick={()=>onFinish()}>
+            See Plan
           </Button>
   )
 }
